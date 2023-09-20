c0f0c14b081f3b982da034228d6aa20fc8a209f3
CHRISTIAN EZEANYAECHE <christianezeanyaeche@gmail.com>
2023-09-20 13:13:07 +0100

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -18,14 +18,14 @@ export const Header = () => {
     return (
         <header className='shadow-md sticky top-0 bg-white left-0'>
             <div className='wrapper flex items-center'>
-                <h1 className='font-bold text-3xl italic mr-6'><span className='text-pink-700 text-5xl'>G</span><span className='hidden min-[420px]:inline-block'>allerie</span></h1>
-                <input type='search' onChange={onChange} value={filter} placeholder='Search Here...' className='ml-auto px-4 py-1 w-full border-[1px] border-[#3333] border-solid outline-none focus:border-[#333e] sm:w-fit sm:mr-auto' />
-                <button className='ml-2 px-2 py-2 rounded-sm active:bg-pink-100 focus:bg-pink-100 sm:hidden'><FaBars className='text-pink-700 text-2xl' onClick={onClick} /></button>
-                <div className='hidden ml-8 sm:flex sm:gap-4 sm:items-center'>
+                <h1 className='font-bold text-3xl italic mr-6 flex flex-nowrap'><span className='text-pink-700 text-5xl'>G</span><span className='hidden min-[420px]:inline-block'>allerie</span></h1>
+                <input type='search' onChange={onChange} value={filter} placeholder='Search Here...' className='ml-auto px-4 py-1 w-full border-[1px] border-[#3333] border-solid outline-none focus:border-[#333e] md:w-fit md:mr-auto' />
+                <button className='ml-2 px-2 py-2 rounded-sm active:bg-pink-100 focus:bg-pink-100 md:hidden'><FaBars className='text-pink-700 text-2xl' onClick={onClick} /></button>
+                <div className='hidden ml-8 md:flex md:gap-4 md:items-center'>
                     <h3>{session?.user?.email}</h3>
                     <button className='ml-4 inline-flex items-center py-1 px-5 bg-pink-700 text-white' onClick={logOut}>Logout <TbLogout className='text-white ml-2' /></button>
                 </div>
-                <div className={`${showMenu ? 'block sm:hidden' : 'hidden'} absolute top-full right-0 min-w-[18rem] bg-white py-8`}>
+                <div className={`${showMenu ? 'block md:hidden' : 'hidden'} absolute top-full right-0 min-w-[18rem] bg-white py-8`}>
                     <div className='pr-[5%] pl-[2rem]'>
                         <h3 className='py-4 w-full'>{session?.user?.email}</h3>
                         <button className='py-4 flex items-center w-full hover:bg-pink-100' onClick={logOut}>Logout <TbLogout className='ml-2 text-2xl' /></button>
